96eae525394529bd3678c644a159e54107982883
Soumya2901roy <59948831+Soumya2901roy@users.noreply.github.com>
2020-03-19 21:54:27 +0530

    Delete getIndiaData.py

--- getIndiaData.py
+++
@@ -1,8 +0,0 @@
-import requests
-import json
-
-url="https://corona.lmao.ninja/countries/India"
-r = requests.get(url)
-countries_data=r.json()
-with open('countries_data.txt', 'w') as json_file:
-  json.dump(countries_data, json_file)
